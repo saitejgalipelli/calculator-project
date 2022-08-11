2c7e1688c620bf7c695e1d6824ce026d3cab70eb
tez-developer <111052106+tez-developer@users.noreply.github.com>
2022-08-11 22:11:59 +0530

    Update Cal.java

--- Cal.java
+++ Cal.java
@@ -1,6 +1,5 @@
 iclass Cal{
 public static void main(String[] args){
-	System.out.println("in main");
 System.out.println(add(2,3));
 System.out.println(mul(2,3));
 // sub
@@ -16,4 +15,4 @@ public static int mul(int i, int i2){
 public static int sub(int i, int i2){
 	return i-i2;
 }
-System.out.println("MAIN ENDED");}
+}
