d59ff955de9b6cadc1f972222c9fda9351058ad6
saitheja.g <saitheja.g@gmail.com>
2022-08-11 21:57:34 +0530

    sop added after main

--- Cal.java
+++ Cal.java
@@ -1,5 +1,6 @@
 iclass Cal{
 public static void main(String[] args){
+	System.out.println("MAIN STARTED");
 System.out.println(add(2,3));
 System.out.println(mul(2,3));
 // sub
@@ -15,5 +16,4 @@ public static int mul(int i, int i2){
 public static int sub(int i, int i2){
 	return i-i2;
 }
-System.out.println("MAIN ENDED");
-}
+System.out.println("MAIN ENDED");}
